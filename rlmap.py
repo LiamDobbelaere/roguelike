05cba6fd90573ad78e4e8073df24db772c56a0b2
Tom Dobbelaere <tom.dobbelaere@outlook.com>
2020-02-29 12:36:21 +0100

    Initial commit

---
+++ rlmap.py
@@ -0,0 +1,234 @@
+# Written in Python 3.8, with numpy and Pillow installed
+
+import numpy as np
+from PIL import Image
+import sys
+
+im = Image.open(sys.argv[1], "r")
+width, height = im.size
+pixel_values = list(im.getdata())
+
+for n in range(0, width * height):
+    pixel_values[n] = pixel_values[n][0] // 255
+
+arrpixels = np.array(pixel_values).reshape((width, height))
+
+# Read the tiles into tiles[], skipping blank tiles
+tiles = []
+for ty in range(0, height, 12):
+    for tx in range(0, width, 12):
+        newtile = []
+        is_filled = False
+        for y in range(0, 12):
+            newtile_row = []
+            for x in range(0, 12):
+                pv = arrpixels[ty + y][tx + x]
+                if pv > 0:
+                    is_filled = True
+                newtile_row.append(pv)
+            newtile.append(newtile_row)
+        if is_filled:
+            tiles.append(newtile)
+
+print(str(len(tiles)) + " tiles read")
+
+# Create an extended tileset by rotating and flipping
+tiles_extended = []
+for tile in tiles:
+    tiles_extended.append(tile)
+    tiles_extended.append(np.rot90(tile, 1))
+    tiles_extended.append(np.rot90(tile, 2))
+    tiles_extended.append(np.rot90(tile, 3))
+
+    flippedud = np.flipud(tile)
+    tiles_extended.append(flippedud)
+    tiles_extended.append(np.rot90(flippedud, 1))
+    tiles_extended.append(np.rot90(flippedud, 2))
+    tiles_extended.append(np.rot90(flippedud, 3))
+
+    flippedlr = np.fliplr(tile)
+    tiles_extended.append(np.rot90(flippedlr, 1))
+    tiles_extended.append(np.rot90(flippedlr, 2))
+    tiles_extended.append(np.rot90(flippedlr, 3))
+
+print(str(len(tiles_extended)) + " extended tiles created")
+
+# Reduce the extended tileset by only taking the unique tiles
+tiles_extended = np.unique(tiles_extended, axis=0)
+
+print(str(len(tiles_extended)) + " unique tiles written")
+
+# Calculate possible neighbouring tiles
+tiles_neighbours = {}
+
+
+def calculateValidConnections(tileA, tileAIndex, tileB, tileBIndex, validatorfunc):
+    if tileAIndex not in tiles_neighbours:
+        tiles_neighbours[tileAIndex] = {
+            "top": [],
+            "right": [],
+            "bottom": [],
+            "left": [],
+        }
+
+    if tileBIndex not in tiles_neighbours:
+        tiles_neighbours[tileBIndex] = {
+            "top": [],
+            "right": [],
+            "bottom": [],
+            "left": [],
+        }
+
+    validation = validatorfunc(tileA, tileB)
+
+    # we always add the reverse relation too, if a.right matches with b.left, then b.left matches with a.right
+    if validation["right"]:
+        tiles_neighbours[tileAIndex]["right"].append(tileBIndex)
+        tiles_neighbours[tileBIndex]["left"].append(tileAIndex)
+
+    if validation["left"]:
+        tiles_neighbours[tileAIndex]["left"].append(tileBIndex)
+        tiles_neighbours[tileBIndex]["right"].append(tileAIndex)
+
+    if validation["bottom"]:
+        tiles_neighbours[tileAIndex]["bottom"].append(tileBIndex)
+        tiles_neighbours[tileBIndex]["top"].append(tileAIndex)
+
+    if validation["top"]:
+        tiles_neighbours[tileAIndex]["top"].append(tileBIndex)
+        tiles_neighbours[tileBIndex]["bottom"].append(tileAIndex)
+
+
+def arrayOrWithValue(a, b, v):
+    for i in range(0, len(a)):
+        if a[i] == v and a[i] == b[i]:
+            return True
+    return False
+
+
+def tile2sides(tile):
+    sides = {}
+
+    sides["top"] = tile[0]
+    sides["bottom"] = tile[len(tile) - 1]
+
+    sides["left"] = []
+    for i in range(0, len(tile)):
+        sides["left"].append(tile[i][0])
+
+    sides["right"] = []
+    for i in range(0, len(tile)):
+        sides["right"].append(tile[i][len(tile[i]) - 1])
+
+    return sides
+
+
+def oneMatchingBorderBlockValidator(tileA, tileB):
+    validation = {"top": False, "right": False, "bottom": False, "left": False}
+
+    aSides = tile2sides(tileA)
+    bSides = tile2sides(tileB)
+
+    if arrayOrWithValue(aSides["left"], bSides["right"], 1):
+        validation["left"] = True
+
+    if arrayOrWithValue(aSides["right"], bSides["left"], 1):
+        validation["right"] = True
+
+    if arrayOrWithValue(aSides["top"], bSides["bottom"], 1):
+        validation["top"] = True
+
+    if arrayOrWithValue(aSides["bottom"], bSides["top"], 1):
+        validation["bottom"] = True
+
+    return validation
+
+
+print("Calculating neighbours...")
+# first time we run through 0 to n, then 1 to n, 2 to n,..
+# this is because with tiles A-B-C, only A-A, A-B, A-C, B-B, B-C, C-C needs to be calculated
+# since the reverse relation can be inferred, A-B = B-A for instance
+for i in range(0, len(tiles_extended)):
+    for j in range(0, len(tiles_extended)):
+        calculateValidConnections(
+            tiles_extended[i], i, tiles_extended[j], j, oneMatchingBorderBlockValidator,
+        )
+        neighbour_count = (
+            len(tiles_neighbours[i]["top"])
+            + len(tiles_neighbours[i]["right"])
+            + len(tiles_neighbours[i]["bottom"])
+            + len(tiles_neighbours[i]["left"])
+        )
+
+min_neighbours = 1000000
+max_neighbours = 0
+for i in tiles_neighbours:
+    neighbour_count = (
+        len(tiles_neighbours[i]["top"])
+        + len(tiles_neighbours[i]["right"])
+        + len(tiles_neighbours[i]["bottom"])
+        + len(tiles_neighbours[i]["left"])
+    )
+
+    if neighbour_count == 0:
+        print("Warning: Tile with index " + str(i) + " has no neighbours!")
+
+    if neighbour_count < min_neighbours:
+        min_neighbours = neighbour_count
+
+    if neighbour_count > max_neighbours:
+        max_neighbours = neighbour_count
+
+print("min_neighbours:" + str(min_neighbours))
+print("max_neighbours:" + str(max_neighbours))
+
+# Write all gathered info to a js file
+with open(sys.argv[1].split(".")[0] + ".rlt.js", "w") as file:
+    file.write("const " + sys.argv[1].split(".")[0] + " = [\n")
+    n = 0
+    for idx, tile in enumerate(tiles_extended):
+        n += 1
+        need_open = True
+        for y in range(0, 12):
+            openb = ""
+            closeb = ""
+            endcomma = ","
+            if need_open:
+                openb = "new Tile(" + str(idx) + ",[\n"
+            if y == 11:
+                topnb = (
+                    ",\n["
+                    + ",".join([str(i) for i in tiles_neighbours[idx]["top"]])
+                    + "]"
+                )
+                rightnb = (
+                    ",\n["
+                    + ",".join([str(i) for i in tiles_neighbours[idx]["right"]])
+                    + "]"
+                )
+                bottomnb = (
+                    ",\n["
+                    + ",".join([str(i) for i in tiles_neighbours[idx]["bottom"]])
+                    + "]"
+                )
+                leftnb = (
+                    ",\n["
+                    + ",".join([str(i) for i in tiles_neighbours[idx]["left"]])
+                    + "]"
+                )
+
+                closeb = "]" + topnb + rightnb + bottomnb + leftnb + ")"
+            if y == 11 and n == len(tiles_extended):
+                endcomma = ""
+
+            file.write(
+                openb
+                + "["
+                + ",".join([str(i) for i in tile[y]])
+                + "]"
+                + closeb
+                + endcomma
+                + "\n"
+            )
+            need_open = False
+    file.write("];")
---
+++ script.js
@@ -0,0 +1,266 @@
+const tiles = tileset1;
+
+function findRandomTileChoices(x, y, level, levelWidth, levelHeight) {
+  let xToRight = x + 1;
+  let xToLeft = x - 1;
+  let yToBottom = y + 1;
+  let yToTop = y - 1;
+
+  console.log({
+    y,
+    xToRight,
+    levelWidth
+  });
+  let tileRight =
+    xToRight >= levelWidth ? undefined : tiles[level[y][xToRight]];
+  let tileLeft = xToLeft < 0 ? undefined : tiles[level[y][xToLeft]];
+  let tileTop = yToTop < 0 ? undefined : tiles[level[yToTop][x]];
+  let tileBottom =
+    yToBottom >= levelHeight ? undefined : tiles[level[yToBottom][x]];
+
+  const rightChoices = tileRight ? tileRight.left : [];
+  const leftChoices = tileLeft ? tileLeft.right : [];
+  const topChoices = tileTop ? tileTop.bottom : [];
+  const bottomChoices = tileBottom ? tileBottom.top : [];
+
+  const totalChoices = Array.from(Array(tiles.length).keys());
+
+  const choices = totalChoices.reduce((acc, currentChoice) => {
+    let shouldAdd = true;
+
+    if (
+      (tileRight && !rightChoices.includes(currentChoice)) ||
+      (tileLeft && !leftChoices.includes(currentChoice)) ||
+      (tileTop && !topChoices.includes(currentChoice)) ||
+      (tileBottom && !bottomChoices.includes(currentChoice))
+    ) {
+      shouldAdd = false;
+    }
+
+    if (shouldAdd) {
+      acc.push(currentChoice);
+    }
+
+    return acc;
+  }, []);
+
+  return choices;
+}
+
+const levelWidth = 24;
+const levelHeight = 24;
+
+const level = new Array(levelHeight);
+const visited = new Array(levelHeight);
+for (let i = 0; i < level.length; i++) {
+  level[i] = new Array(levelWidth);
+  visited[i] = new Array(levelWidth);
+}
+
+let startX = Math.floor(levelWidth / 2);
+let startY = Math.floor(levelHeight / 2);
+let addedTiles = 1;
+
+let nextTile = [[startY, startX, "none"]];
+visited[startY][startX] = true;
+let firstTime = true;
+
+const tileset = document.getElementById("tileset");
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+
+let camX = 0;
+
+ctx.imageSmoothingEnabled = false;
+ctx.scale(2, 2);
+ctx.translate(0, 0);
+
+let simulateSlow = true;
+
+while (!simulateSlow && nextTile.length) {
+  generate();
+}
+
+function generate() {
+  let [y, x] = nextTile.pop();
+
+  let xToRight = x + 1;
+  let xToLeft = x - 1;
+  let yToBottom = y + 1;
+  let yToTop = y - 1;
+
+  // Generate new tile based on the options
+  const tileChoices = findRandomTileChoices(
+    x,
+    y,
+    level,
+    levelWidth,
+    levelHeight
+  );
+
+  let randomTile = tileChoices[Math.floor(Math.random() * tileChoices.length)];
+
+  if (!randomTile) {
+    if (firstTime) {
+      randomTile = Math.floor(Math.random() * tiles.length);
+    } else {
+      console.warn(
+        "I'm stuck, they call me gaseous clay. Leaving tile blank :("
+      );
+      return;
+    }
+  }
+
+  level[y][x] = randomTile;
+
+  let scheduledNextTiles = [];
+
+  const topEntropy = findRandomTileChoices(
+    x,
+    yToTop,
+    level,
+    levelWidth,
+    levelHeight
+  ).length;
+  const rightEntropy = findRandomTileChoices(
+    xToRight,
+    y,
+    level,
+    levelWidth,
+    levelHeight
+  ).length;
+  const bottomEntropy = findRandomTileChoices(
+    x,
+    yToBottom,
+    level,
+    levelWidth,
+    levelHeight
+  ).length;
+  const leftEntropy = findRandomTileChoices(
+    xToLeft,
+    y,
+    level,
+    levelWidth,
+    levelHeight
+  ).length;
+
+  console.log({
+    topEntropy,
+    leftEntropy,
+    rightEntropy,
+    bottomEntropy
+  });
+  const checks = shuffle([
+    () => {
+      if (topEntropy && yToTop >= 0 && !visited[yToTop][x]) {
+        scheduledNextTiles.push([yToTop, x, topEntropy]);
+        visited[yToTop][x] = true;
+
+        addedTiles += 1;
+        return true;
+      }
+
+      return false;
+    },
+    () => {
+      if (rightEntropy && xToRight < levelWidth && !visited[y][xToRight]) {
+        scheduledNextTiles.push([y, xToRight, rightEntropy]);
+        visited[y][xToRight] = true;
+
+        addedTiles += 1;
+        return true;
+      }
+
+      return false;
+    },
+    () => {
+      if (bottomEntropy && yToBottom < levelHeight && !visited[yToBottom][x]) {
+        scheduledNextTiles.push([yToBottom, x, bottomEntropy]);
+        visited[yToBottom][x] = true;
+
+        addedTiles += 1;
+        return true;
+      }
+
+      return false;
+    },
+    () => {
+      if (leftEntropy && xToLeft >= 0 && !visited[y][xToLeft]) {
+        scheduledNextTiles.push([y, xToLeft, leftEntropy]);
+        visited[y][xToLeft] = true;
+
+        addedTiles += 1;
+        return true;
+      }
+
+      return false;
+    }
+  ]);
+
+  let lastResult = false;
+  let callIdx = 0;
+  while (!lastResult && callIdx < checks.length) {
+    lastResult = checks[callIdx]();
+    callIdx++;
+  }
+
+  scheduledNextTiles.sort((a, b) => {
+    // Sort by lowest entropy first
+    return b[2]() - a[2]();
+  });
+
+  scheduledNextTiles.forEach(tile => {
+    tile.pop(); // Remove entropy value
+    nextTile.push(tile);
+  });
+
+  firstTime = false;
+}
+
+const interval = setInterval(() => {
+  if (!nextTile.length) {
+    //clearInterval(interval);
+    //console.log(level);
+
+    redrawCanvas();
+
+    return;
+  }
+
+  if (simulateSlow) {
+    generate();
+  }
+
+  redrawCanvas();
+}, 10);
+
+function shuffle(a) {
+  var j, x, i;
+  for (i = a.length - 1; i > 0; i--) {
+    j = Math.floor(Math.random() * (i + 1));
+    x = a[i];
+    a[i] = a[j];
+    a[j] = x;
+  }
+  return a;
+}
+
+function redrawCanvas() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  for (let y = 0; y < level.length; y++) {
+    for (let x = 0; x < level[y].length; x++) {
+      const currentTile = tiles[level[y][x]];
+
+      if (currentTile) {
+        for (let yy = 0; yy < 12; yy++) {
+          for (let xx = 0; xx < 12; xx++) {
+            if (currentTile.data[yy][xx]) {
+              ctx.fillRect(x * 12 + xx, y * 12 + yy, 1, 1);
+            }
+          }
+        }
+      }
+    }
+  }
+}
